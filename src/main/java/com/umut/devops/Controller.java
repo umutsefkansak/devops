c92cda2386969d77b6f9034df1eed811ee1def8c
Umut Sefkan SAK <109917881+umutsefkansak@users.noreply.github.com>
2024-04-30 18:28:51 +0300

    firs commit

---
+++ src/main/java/com/umut/devops/Controller.java
@@ -0,0 +1,29 @@
+package com.umut.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/getProjeBilgileri")
+    public String getProjeBilgileri(){
+        return "Odev: Spring boot github jenkins ve docker baglantisi\n";
+    }
+
+
+    @GetMapping("/getIsimler")
+    public String getIsimler(){
+        return "Umut Sefkan SAK <br></br> Samet EROĞLU <br></br> Taşkın KIŞLAK <br></br> ";
+    }
+
+    @GetMapping("/getAdminBilgileri")
+    public String getAdminBilgileri(){
+        return "Umut Sefkan Sak\n Yazilim Muhendisligi 3. Sınıf\n 02210224071";
+    }
+
+
+
+}
